b754d94adb531fe19e136cd4a33f5426b7ede5bd
Sunjida-Srijoni <sunjidasrijoni@gmail.com>
2024-03-01 00:13:24 +0600

    dynamically card data has been added

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
-const posts = [
-    {
-        title: "this is title 1",
-        body: "this is body 1"
-    },
-    {
-        title: "this is title 2",
-        body: "this is body 2"
-    },
-    {
-        title: "this is title 3",
-        body: "this is body 3"
-    },
-    {
-        title: "this is title 4",
-        body: "this is body 4"
+// const posts = [
+//     {
+//         title: "this is title 1",
+//         body: "this is body 1"
+//     },
+//     {
+//         title: "this is title 2",
+//         body: "this is body 2"
+//     },
+//     {
+//         title: "this is title 3",
+//         body: "this is body 3"
+//     },
+//     {
+//         title: "this is title 4",
+//         body: "this is body 4"
+//     }
+// ];
+
+//console.log(axios);
+
+const fetchData = async (config) => {
+    try{
+        const res = await axios(config);
+    console.log(res);
+    return res.data;
     }
-];
+    catch(error){
+        throw Error("Data is not fetched");
+    }
+
+};
+
 
 const postsElement = document.querySelector(".posts");
 
-const loadAllData = () => {
+const loadAllData = async () => {
+    const posts = await fetchData("https://jsonplaceholder.typicode.com/posts");
      posts.map(post => {
-        console.log(post);
+        //console.log(post);
         const postElement = document.createElement("div");
         postElement.classList.add("post");
         postElement.innerHTML = `<h4 class="post-title">${post.title}</h4>
